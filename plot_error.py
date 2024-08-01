3584678bce27a5efc0cc0c558d7033ef103121f2
kylajones <kjones29@nd.edu>
2024-08-01 15:13:18 -0500

    updated figure

--- plot_error.py
+++ plot_error.py
@@ -9,12 +9,12 @@ rcParams.set_plot_settings()
 mysalmon = (235/255, 134/255, 100/255)
 myteal = (103/225, 185/255, 155/255)
 
-xlabels = ['$H_{vap}$'+'\n [kJ '+r'$\cdot$'+' mol' + '$^{-1}$'+']',
-           '$P_c$'+'\n [bar]',
-           '$T_b$'+'\n [K]',
-           '$T_c$'+'\n [K]',
-           '$T_m$'+'\n [K]',
-           '$V_c$'+'\n [cm'+'$^{3}$'+']']
+xlabels = ['$H_{vap}$'+'\n [kJ '+'$\cdot$'+' mol' + '$^{-1}$'+']',
+           '$P_c$'+' [bar]',
+           '$T_b$'+' [K]',
+           '$T_c$'+' [K]',
+           '$T_m$'+' [K]',
+           '$V_c$'+'\n [cm'+'$^{3}$'+'$\cdot$'+' mol' + '$^{-1}$'+']']
 
 # Set path to results
 train = pd.read_csv(os.path.join('Final_Results', 'train_error'), index_col = 0)
@@ -23,29 +23,21 @@ width = 0.2
 no_cats = len(xlabels)
 x = np.arange(no_cats)
 
-plt.figure()
+plt.figure(figsize=(10,6))
 plt.bar(x - 1.5*width, train.loc['MAE'],  width,  color = mysalmon, edgecolor = 'k')
-plt.bar(x - width/2,   test.loc['MAE'],   width,  color = mysalmon, edgecolor = 'k',  hatch = '/')
+plt.bar(x - width/2,   test.loc['MAE'],   width,  color = mysalmon, edgecolor = 'k',  hatch = '//')
 
 plt.bar(x + width/2,   train.loc['RMSE'],  width,  color = myteal, edgecolor = 'k')
-plt.bar(x + 1.5*width, test.loc['RMSE'],  width, color = myteal, edgecolor = 'k', hatch = '/')
+plt.bar(x + 1.5*width, test.loc['RMSE'],  width, color = myteal, edgecolor = 'k', hatch = '//')
 
 # For the legends
 plt.bar(x - 1.5*width, 0*train.loc['MAE'],  width,  color = mysalmon, edgecolor = 'k', label = 'MAE')
 plt.bar(x - 1.5*width, 0*train.loc['MAE'],  width,  color = myteal, edgecolor = 'k', label = 'RMSE')
 plt.bar(x + width/2,   0*test.loc['MAE'],   width,  color = 'w', edgecolor = 'k',  label = 'Train')
-plt.bar(x + width/2,   0*test.loc['MAE'],   width,  color = 'w', edgecolor = 'k',  hatch = '/', label = 'Test')
+plt.bar(x + width/2,   0*test.loc['MAE'],   width,  color = 'w', edgecolor = 'k',  hatch = '//', label = 'Test')
 
 plt.xticks(x, labels = xlabels)
 plt.xlabel("Thermophysical Property")
 plt.ylabel("Error")
 plt.legend(loc = 'upper left')
 plt.savefig(os.path.join('Final_Results', 'error_bar_chart'))
-
-# plt.figure()
-
-# plt.xticks(x, labels = xlabels)
-# plt.xlabel("Thermophysical Property")
-# plt.ylabel("RMSE")
-# plt.legend()
-# plt.savefig(os.path.join('Final_Results', 'rmse_bar_chart'))

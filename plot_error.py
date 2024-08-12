8c16e29642a979e885d0e7b54899de858b37a3b1
kylajones <kjones29@nd.edu>
2024-08-12 18:02:37 -0400

    created shared axis

--- plot_error.py
+++ plot_error.py
@@ -9,11 +9,11 @@ rcParams.set_plot_settings()
 
 # Define custom colors
 mysalmon = (235/255, 134/255, 100/255)
-myteal   = (103/255, 185/255, 155/255)
+myteal = (103/255, 185/255, 155/255)
 
 # Define labels for the x-axis
 xlabels = [
-    '$H_{vap}$' + '\n [kJ ' + '$\cdot$' + ' mol' + '$^{-1}$' + ']',
+    '$H_{vap}$' + '\n[kJ $\cdot$' + ' mol' + '$^{-1}$' + ']',
     '$P_c$' + ' [bar]',
     '$T_b$' + ' [K]',
     '$T_c$' + ' [K]',
@@ -21,8 +21,8 @@ xlabels = [
     '$V_c$' + '\n [cm' + '$^{3}$' + '$\cdot$' + ' mol' + '$^{-1}$' + ']'
 ]
 
-def plot_error_bars(train_file, test_file, output_file, xlabels, width=0.2):
-    """Plots error bars for the train and test datasets."""
+def plot_error_bars(ax, train_file, test_file, xlabels, width=0.2, ylabel=True, show_legend=False):
+    """Plots error bars for the train and test datasets on a given axis."""
     # Load data
     train = pd.read_csv(train_file, index_col=0)
     test = pd.read_csv(test_file, index_col=0)
@@ -30,44 +30,58 @@ def plot_error_bars(train_file, test_file, output_file, xlabels, width=0.2):
     no_cats = len(xlabels)
     x = np.arange(no_cats)
     
-    # Create the bar plot
-    plt.figure(figsize=(10, 6))
-    
     # Plot MAE bars
-    plt.bar(x - 1.5 * width, train.loc['MAE'], width, color = mysalmon, edgecolor='k')
-    plt.bar(x - 0.5 * width, test.loc['MAE'], width, color = mysalmon, edgecolor='k', hatch='//')
+    bars1 = ax.bar(x - 1.5*width, train.loc['MAE'], width, color=mysalmon, edgecolor='k', label='Train MAE')
+    bars2 = ax.bar(x - width/2, test.loc['MAE'], width, color=mysalmon, edgecolor='k', hatch='//', label='Test MAE')
     
     # Plot RMSE bars
-    plt.bar(x + 0.5 * width, train.loc['RMSE'], width, color = myteal, edgecolor = 'k')
-    plt.bar(x + 1.5 * width, test.loc['RMSE'], width, color = myteal, edgecolor = 'k', hatch = '//')
+    bars3 = ax.bar(x + width/2, train.loc['RMSE'], width, color=myteal, edgecolor='k', label='Train RMSE')
+    bars4 = ax.bar(x + 1.5*width, test.loc['RMSE'], width, color=myteal, edgecolor='k', hatch='//', label='Test RMSE')
     
-    # Add legends
-    plt.bar(x - 1.5 * width, 0, width, color=mysalmon, edgecolor='k', label='MAE')
-    plt.bar(x - 1.5 * width, 0, width, color=myteal, edgecolor='k', label='RMSE')
-    plt.bar(x + 0.5 * width, 0, width, color='w', edgecolor='k', label='Train')
-    plt.bar(x + 0.5 * width, 0, width, color='w', edgecolor='k', hatch='//', label='Test')
+    # Add error values on top of bars with rotation and consistent formatting
+    for bars in [bars1, bars2, bars3, bars4]:
+        for bar in bars:
+            yval = bar.get_height()
+            ax.text(bar.get_x() + bar.get_width()/2, yval + 3, "{:.2f}".format(yval), 
+                    ha='center', va='bottom', fontsize=9, rotation=90)
     
     # Set labels and titles
-    plt.xticks(x, labels = xlabels)
-    plt.xlabel("Thermophysical Property")
-    plt.ylabel("Error")
-    plt.legend(loc = 'upper left')
+    ax.set_xticks(x)
+    ax.set_xticklabels(xlabels)
+    ax.set_xlabel("Thermophysical Property")
+    if ylabel:
+        ax.set_ylabel("Error")
     
-    # Save the plot to a file
+    # Rotate x-axis labels
+    ax.tick_params(axis='x', labelrotation=60)
+    
+    # Add legend only if requested
+    if show_legend:
+        ax.legend(loc='upper right')
+
+def create_shared_yaxis_plots(train_file_1, test_file_1, train_file_2, test_file_2, xlabels, output_file, width=0.2):
+    """Creates two subplots with a shared y-axis."""
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 6), sharey=True)
+    
+    # Plot the Joback model results on the left axis with legend
+    plot_error_bars(ax1, train_file_2, test_file_2, xlabels, width, show_legend=False)
+    ax1.text(-0.35, 155, "(a) Joback", fontsize = 14, fontweight = 'bold')
+    
+    # Plot the model with discrepancy correction on the right axis with the legend
+    plot_error_bars(ax2, train_file_1, test_file_1, xlabels, width, ylabel=False, show_legend=True)
+    ax2.text(-0.35, 155, "(b) Hybrid", fontsize = 14, fontweight = 'bold')
+    
+    ax1.set_ylim(0, 170)
+    # Save the figure to a file
+    plt.tight_layout()
     plt.savefig(output_file)
 
-# Plot the first set of results
-plot_error_bars(
-    train_file=os.path.join('Final_Results', 'train_error'),
-    test_file=os.path.join('Final_Results', 'test_error'),
-    output_file=os.path.join('Final_Results', 'error_bar_chart'),
-    xlabels=xlabels
-)
-
-# Plot the Joback model results
-plot_error_bars(
-    train_file=os.path.join('Final_Results', 'train_error_jb'),
-    test_file=os.path.join('Final_Results', 'test_error_jb'),
-    output_file=os.path.join('Final_Results', 'error_bar_chart_jb'),
-    xlabels=xlabels
+# Create the shared y-axis plots
+create_shared_yaxis_plots(
+    train_file_1=os.path.join('Final_Results', 'train_error'),
+    test_file_1=os.path.join('Final_Results', 'test_error'),
+    train_file_2=os.path.join('Final_Results', 'train_error_jb'),
+    test_file_2=os.path.join('Final_Results', 'test_error_jb'),
+    xlabels=xlabels,
+    output_file=os.path.join('Final_Results', 'error_bar_chart_shared_yaxis')
 )

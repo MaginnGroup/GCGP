34301ca5111e13444b02133f08e7e1d76cba6faa
kylajones <kjones29@nd.edu>
2024-08-12 17:16:14 -0400

    added joback error metrics

--- plot_error.py
+++ plot_error.py
@@ -4,40 +4,70 @@ import os
 import matplotlib.pyplot as plt
 import global_plot_settings as rcParams
 
+# Set global plot settings
 rcParams.set_plot_settings()
 
+# Define custom colors
 mysalmon = (235/255, 134/255, 100/255)
-myteal = (103/225, 185/255, 155/255)
+myteal   = (103/255, 185/255, 155/255)
 
-xlabels = ['$H_{vap}$'+'\n [kJ '+'$\cdot$'+' mol' + '$^{-1}$'+']',
-           '$P_c$'+' [bar]',
-           '$T_b$'+' [K]',
-           '$T_c$'+' [K]',
-           '$T_m$'+' [K]',
-           '$V_c$'+'\n [cm'+'$^{3}$'+'$\cdot$'+' mol' + '$^{-1}$'+']']
+# Define labels for the x-axis
+xlabels = [
+    '$H_{vap}$' + '\n [kJ ' + '$\cdot$' + ' mol' + '$^{-1}$' + ']',
+    '$P_c$' + ' [bar]',
+    '$T_b$' + ' [K]',
+    '$T_c$' + ' [K]',
+    '$T_m$' + ' [K]',
+    '$V_c$' + '\n [cm' + '$^{3}$' + '$\cdot$' + ' mol' + '$^{-1}$' + ']'
+]
 
-# Set path to results
-train = pd.read_csv(os.path.join('Final_Results', 'train_error'), index_col = 0)
-test  = pd.read_csv(os.path.join('Final_Results', 'test_error'),  index_col = 0)
-width = 0.2
-no_cats = len(xlabels)
-x = np.arange(no_cats)
+def plot_error_bars(train_file, test_file, output_file, xlabels, width=0.2):
+    """Plots error bars for the train and test datasets."""
+    # Load data
+    train = pd.read_csv(train_file, index_col=0)
+    test = pd.read_csv(test_file, index_col=0)
+    
+    no_cats = len(xlabels)
+    x = np.arange(no_cats)
+    
+    # Create the bar plot
+    plt.figure(figsize=(10, 6))
+    
+    # Plot MAE bars
+    plt.bar(x - 1.5 * width, train.loc['MAE'], width, color = mysalmon, edgecolor='k')
+    plt.bar(x - 0.5 * width, test.loc['MAE'], width, color = mysalmon, edgecolor='k', hatch='//')
+    
+    # Plot RMSE bars
+    plt.bar(x + 0.5 * width, train.loc['RMSE'], width, color = myteal, edgecolor = 'k')
+    plt.bar(x + 1.5 * width, test.loc['RMSE'], width, color = myteal, edgecolor = 'k', hatch = '//')
+    
+    # Add legends
+    plt.bar(x - 1.5 * width, 0, width, color=mysalmon, edgecolor='k', label='MAE')
+    plt.bar(x - 1.5 * width, 0, width, color=myteal, edgecolor='k', label='RMSE')
+    plt.bar(x + 0.5 * width, 0, width, color='w', edgecolor='k', label='Train')
+    plt.bar(x + 0.5 * width, 0, width, color='w', edgecolor='k', hatch='//', label='Test')
+    
+    # Set labels and titles
+    plt.xticks(x, labels = xlabels)
+    plt.xlabel("Thermophysical Property")
+    plt.ylabel("Error")
+    plt.legend(loc = 'upper left')
+    
+    # Save the plot to a file
+    plt.savefig(output_file)
 
-plt.figure(figsize=(10,6))
-plt.bar(x - 1.5*width, train.loc['MAE'],  width,  color = mysalmon, edgecolor = 'k')
-plt.bar(x - width/2,   test.loc['MAE'],   width,  color = mysalmon, edgecolor = 'k',  hatch = '//')
+# Plot the first set of results
+plot_error_bars(
+    train_file=os.path.join('Final_Results', 'train_error'),
+    test_file=os.path.join('Final_Results', 'test_error'),
+    output_file=os.path.join('Final_Results', 'error_bar_chart'),
+    xlabels=xlabels
+)
 
-plt.bar(x + width/2,   train.loc['RMSE'],  width,  color = myteal, edgecolor = 'k')
-plt.bar(x + 1.5*width, test.loc['RMSE'],  width, color = myteal, edgecolor = 'k', hatch = '//')
-
-# For the legends
-plt.bar(x - 1.5*width, 0*train.loc['MAE'],  width,  color = mysalmon, edgecolor = 'k', label = 'MAE')
-plt.bar(x - 1.5*width, 0*train.loc['MAE'],  width,  color = myteal, edgecolor = 'k', label = 'RMSE')
-plt.bar(x + width/2,   0*test.loc['MAE'],   width,  color = 'w', edgecolor = 'k',  label = 'Train')
-plt.bar(x + width/2,   0*test.loc['MAE'],   width,  color = 'w', edgecolor = 'k',  hatch = '//', label = 'Test')
-
-plt.xticks(x, labels = xlabels)
-plt.xlabel("Thermophysical Property")
-plt.ylabel("Error")
-plt.legend(loc = 'upper left')
-plt.savefig(os.path.join('Final_Results', 'error_bar_chart'))
+# Plot the Joback model results
+plot_error_bars(
+    train_file=os.path.join('Final_Results', 'train_error_jb'),
+    test_file=os.path.join('Final_Results', 'test_error_jb'),
+    output_file=os.path.join('Final_Results', 'error_bar_chart_jb'),
+    xlabels=xlabels
+)
--- uncertainty_analysis.py
+++ uncertainty_analysis.py
@@ -12,6 +12,7 @@ results = os.path.join(os.getcwd(), 'Final_Results')
 
 ## Initialize dictionaries to save results to
 test_stats, train_stats = {}, {}
+test_stats_jb, train_stats_jb = {}, {}
 
 # Loop over thermophysical properties
 for loopA, property in enumerate(phys_property):
@@ -22,23 +23,39 @@ for loopA, property in enumerate(phys_property):
         # Read in files
         observations    = np.loadtxt(os.path.join(results, property, property + '_Y_'+ set_type +'_true.txt'))
         predictions     = np.loadtxt(os.path.join(results, property, property + '_Y_'+ set_type +'_pred.txt'))
-
-        # Compute RMSE and MAE 
+        predictions_jb  = np.loadtxt(os.path.join(results, property, 'model_4', set_type + '_data.csv'), skiprows = 1, delimiter=',')[:,1]
+        
+        # Compute RMSE and MAE for GP model
         mae  = myFxns.MAE(observations, predictions)
         rmse = myFxns.RMSE(observations, predictions)
+
+        # Compute RMSE and MAE for Joback model
+        mae_jb  = myFxns.MAE(observations, predictions_jb)
+        rmse_jb = myFxns.RMSE(observations, predictions_jb)
 
         # Save metrics to relevant dictionary
         if set_type == 'train':
             train_stats[property]   = {'n':     len(observations), 
                                        'MAE':   mae, 
                                        'RMSE':  rmse}
+            
+            train_stats_jb[property] = {'n':     len(observations), 
+                                       'MAE':   mae_jb, 
+                                       'RMSE':  rmse_jb}
         elif set_type == 'test':
             test_stats[property]    = {'n':     len(observations), 
                                        'MAE':   mae, 
                                        'RMSE':  rmse}
+            
+            test_stats_jb[property] = {'n':     len(observations), 
+                                       'MAE':   mae_jb, 
+                                       'RMSE':  rmse_jb}
 
 train_error = pd.DataFrame(train_stats).to_csv(os.path.join(results, 'train_error'))
 test_error  = pd.DataFrame(test_stats).to_csv(os.path.join(results, 'test_error'))
+
+train_error_jb = pd.DataFrame(train_stats_jb).to_csv(os.path.join(results, 'train_error_jb'))
+test_error_jb  = pd.DataFrame(test_stats_jb).to_csv(os.path.join(results, 'test_error_jb'))
 
 
 # credible_int    = np.loadtxt(os.path.join(results, property, property + '_Y_'+ set_type +'_pred_95CI.txt'))
